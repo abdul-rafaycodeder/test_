c7185a81cf85d9a95da46d3eb3494c15354a46d6
abdul rafay <abdulrafaycodeder>
2026-01-09 14:40:27 -0800

    coding kar raha ho ma

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-// function popup() {
-//     var popup = document.getElementById("popup").innerText = "kardo app ka naam kya hai mara naa hi pata";
-//     console.log(popup)
+// // function popup() {
+// //     var popup = document.getElementById("popup").innerText = "kardo app ka naam kya hai mara naa hi pata";
+// //     console.log(popup)
+// // }
+
+
+// function expent() {
+//     var expent = document.getElementById("expent").innerText = "kardo app ka naam kya hai mara naa hi pata";
+//     console.log(expent)
 // }
 
 
-function expent() {
-    var expent = document.getElementById("expent").innerText = "kardo app ka naam kya hai mara naa hi pata";
-    console.log(expent)
+
+// function img() {
+//     var image = document.getElementById("img").src = "https://mgmotors.com.pk/_next/image?url=%2Fassets%2Foptimized-imgs%2FMG4-crop_11zon.webp&w=1080&q=75"; 
+
+//     if (!image === true) {
+//         image = document.getElementById("img").src = " https://img.freepik.com/free-psd/red-isolated-car_23-2151852884.jpg?semt=ais_hybrid&w=740&q=80";
+//     } 
+
+
+//     console.log(image)
+// }
+
+
+function checkEmail(emailID) {
+    var email = document.getElementById(emailID).value; 
+    // if (email === "") {
+    //     alert("please enter your email")x
+    // } else if (email === "rafay@email.com") {
+    //     console.log("your email is not correct==>", email)
+    // } else {
+    //     console.log("your email is correct==>", email)
+    // }
+    console.log("Email==>", email)
+    console.log("emailID==>", emailID)
 }
 
 
+// function clickH1TagColor() {
+//     var h1Tag = document.getElementById("h1");
+//     h1Tag.style.color = "red";
+//     h1Tag.style.backgroundColor = "yellow";  
+//     h1Tag.innerHTML = "<h6>you have clicked me</h6>";
 
-function img() {
-    var image = document.getElementById("img").src = "https://mgmotors.com.pk/_next/image?url=%2Fassets%2Foptimized-imgs%2FMG4-crop_11zon.webp&w=1080&q=75"; 
-
-    if (!image === true) {
-        image = document.getElementById("img").src = " https://img.freepik.com/free-psd/red-isolated-car_23-2151852884.jpg?semt=ais_hybrid&w=740&q=80";
-    } 
-
-    
-    console.log(image)
-}
+//     console.log("h1Tag==>", h1Tag)
+// }
