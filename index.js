132d70ee781a75b1c38c11105ef34f6bda2d43e0
abdul rafay <abdulrafaycodeder>
2026-01-09 22:03:52 -0800

    complete chapter 50

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// // // function popup() {
-// // //     var popup = document.getElementById("popup").innerText = "kardo app ka naam kya hai mara naa hi pata";
-// // //     console.log(popup)
-// // // }
-
-
-// // function expent() {
-// //     var expent = document.getElementById("expent").innerText = "kardo app ka naam kya hai mara naa hi pata";
-// //     console.log(expent)
-// // }
-
-
-
-// // function img() {
-// //     var image = document.getElementById("img").src = "https://mgmotors.com.pk/_next/image?url=%2Fassets%2Foptimized-imgs%2FMG4-crop_11zon.webp&w=1080&q=75"; 
-
-// //     if (!image === true) {
-// //         image = document.getElementById("img").src = " https://img.freepik.com/free-psd/red-isolated-car_23-2151852884.jpg?semt=ais_hybrid&w=740&q=80";
-// //     } 
-
-
-// //     console.log(image)
-// // }
-
-
 function checkEmail(emailID) {
     var email = document.getElementById(emailID).value;
     if (email === "") {
@@ -37,14 +12,29 @@ function checkEmail(emailID) {
 }
 
 
-// // function clickH1TagColor() {
-// //     var h1Tag = document.getElementById("h1");
-// //     h1Tag.style.color = "red";
-// //     h1Tag.style.backgroundColor = "yellow";  
-// //     h1Tag.innerHTML = "<h6>you have clicked me</h6>";
 
-// //     console.log("h1Tag==>", h1Tag)
-// // }
+function checkCampus() {
+    var rollNumber = document.getElementById("rollNumber").value;
+    var campus = "";
 
+    switch (rollNumber) {
+        case "100":
+            campus = "zaiton ashraf campus";
+            break;
+        case "200":
+            campus = "gunshan campus";
+            break; 
+        case "300":
+            campus = "bahadurabad campus";
+            break; 
+        default:
+            campus = "you are not inrolled";
+            break;
+    }
+
+    document.getElementById("campus").value = campus;
+
+        console.log("campus==>", campus)
+}
 
 
