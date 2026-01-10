05c41f387180a301fb2ae62c7e6e9365833a8fc1
abdul rafay <abdulrafaycodeder>
2026-01-10 04:56:45 -0800

    complete chapter 52

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function checkEmail(emailID) {
     } else if (email.includes("@") === false) {
         alert("your email is not correct, missing @ symbol")
     } else {
-        alert("your email is correct", email)
+        alert("your email is correct: " + email);
     }
     console.log("Email==>", email)
     console.log("emailID==>", emailID)
@@ -23,28 +23,28 @@ function checkCampus() {
             break;
         case "200":
             campus = "gunshan campus";
-            break; 
+            break;
         case "300":
             campus = "bahadurabad campus";
-            break; 
+            break;
         default:
-            campus = "you are not inrolled";
+            campus = "you are not enrolled";
             break;
     }
 
     document.getElementById("campus").value = campus;
 
-        console.log("campus==>", campus)
+    console.log("campus==>", campus)
 }
 
 var paragraph = false;
 
 function expentpara() {
-    var expentpara = 'Ya hai click karna k bad ka text'
+    var expandedText = 'Ya hai click karna k bad ka text';
 
-    if(paragraph === false) {
-        document.getElementById("expent").innerHTML = expentpara +   for Less</em></a>';
-        paragraph = true;'<a href="#" onclick="expentpara()"><em> Click
+    if (!paragraph) {
+        document.getElementById("expent").innerHTML = expandedText + ' <a href="#" onclick="expentpara()"><em> Click for Less</em></a>';
+        paragraph = true;
     } else {
         document.getElementById("expent").innerHTML = ' This is a small information <a href="#" onclick="expentpara()"><em> Click for more</em></a>';
         paragraph = false;
@@ -54,12 +54,12 @@ function expentpara() {
 
 var oderList = false;
 
-function list(){
+function list() {
     var listItems = '<li>Rafay</li><li>Ali</li><li>Osama</li><li>bilal</li>';
     var oderListH1 = '<h1>Oder List</h1>';
-    var buttonTextchange = 'Click to Hide Oder List'; ;
-   
-    if(oderList === false){
+    var buttonTextchange = 'Click to Hide Oder List';
+
+    if (oderList === false) {
         document.getElementById("oderList").innerHTML = oderListH1 + listItems;
         document.getElementById("changeOderListButtonText").innerText = buttonTextchange;
         oderList = true;
@@ -68,9 +68,51 @@ function list(){
         document.getElementById("changeOderListButtonText").innerText = 'Click to get oder list ';
         oderList = false;
     }
-    
+
 }
 
-function changeImage() {
-    document.getElementById("carImage").className  = "carImage";
+var oderList = false;
+
+function listImage() {
+    var listItems = '<img src="https://images.unsplash.com/photo-1614200179396-2bdb77ebf81b?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bHV4dXJ5JTIwY2FyfGVufDB8fDB8fHww" alt="Car image" width="200px">';
+    var oderListH1 = '<h1>Image</h1>';
+    var buttonTextchange = 'Click to Hide Image';
+
+    if (oderList === false) {
+        document.getElementById("oderListImage").innerHTML = oderListH1 + listItems;
+        document.getElementById("changeOderListButtonImage").innerText = buttonTextchange;
+        oderList = true;
+    } else {
+        document.getElementById("oderListImage").innerHTML = '';
+        document.getElementById("changeOderListButtonImage").innerText = 'Click to get images ';
+        oderList = false;
+    }
+
 }
+
+// first car image hide and show
+
+function hideImage() {
+    document.getElementById("hideImage").className = "imageHide";
+}
+
+
+
+function showImage() {
+    document.getElementById("hideImage").className = "";
+}
+
+// second image big and small on hover
+
+function bigImage() {
+    var image = document.getElementById("imageHideShow");
+    image.className = "bigImage";
+    console.log("image==>", image)
+}
+
+function smallImage() {
+    var image = document.getElementById("imageHideShow");
+    image.className = "smallImage";
+    console.log("image==>", image)
+}
+
